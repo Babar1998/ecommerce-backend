aac0d0c2e8ac6ac6d8320c50d341dfbdd7b1c951
Babar Arif <babararifmasood98@gmail.com>
2023-11-17 00:27:37 +0500

    User CRUD

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,10 @@ var router = express.Router();
 const users = require("../controller/userController");
 
 /* GET users listing. */
-router.get('/', users.userController);
+router.get("/", users.usersController);
+router.post("/add", users.addUserController);
+router.put("/update/:id",users.updatedUserController);
+router.delete("/delete/:id",users.deleteUserController);
+
 
 module.exports = router;
