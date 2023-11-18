13179199b2a03a8067a0d88c3dfc0c8606a781c0
Babar Arif <babararifmasood98@gmail.com>
2023-11-18 20:40:09 +0500

    Relation Created

--- models/index.js
+++ models/index.js
@@ -4,7 +4,14 @@ const category = require("../models/schemas/categorySchema");
 const product = require("../models/schemas/productSchema");
 const sequelize = require("../common/dbconnection");
 
+user.hasMany(cart, {onDelete: "CASCADE", foreignKey:{name: "userID"}})
+cart.belongsTo(user, {onDelete: "CASCADE", foreignKey:{name: "userID"}})
 
+category.hasMany(product, {onDelete: "CASCADE", foreignKey:{name: "categoryID"}})
+product.belongsTo(category, {onDelete: "CASCADE", foreignKey:{name: "categoryID"}})
+
+product.belongsToMany(cart,{through: "product_cart", foreignKey: {name: "productID"}, })
+cart.belongsToMany(product,{through: "product_cart", foreignKey: {name: "cartID"},}) 
 
 const models = sequelize.models;
 console.log(models);
